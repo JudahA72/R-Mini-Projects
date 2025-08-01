ef7c02d3a49b50c71b3576da21a856ee0eaa85d8
JudahA72 <Judahalter@gmail.com>
2025-08-01 16:05:48 -0400

    update

--- Assignment10/Assignment10.R
+++ Assignment10/Assignment10.R
@@ -51,33 +51,3 @@ ci_basic <- 2*corr_orig - rev(ci_percentile)
 cat("95% Percentile CI:", ci_percentile, "\n")
 cat("95% Basic CI:", ci_basic, "\n")
 
-# 2a. Proportion barplot of clarity
-library(ggplot2)
-data(diamonds)
-diamonds_clarity <- as.data.frame(table(diamonds$clarity))
-diamonds_clarity$prop <- diamonds_clarity$Freq / sum(diamonds_clarity$Freq)
-ggplot(diamonds_clarity, aes(x=Var1, y=prop)) +
-  geom_bar(stat="identity", fill="grey40") +
-  labs(x="clarity", y="prop", title="Proportion of diamonds by clarity")
-
-# 2b. Count barplot of clarity colored by clarity
-ggplot(diamonds, aes(x=clarity, fill=clarity)) +
-  geom_bar() +
-  labs(x="clarity", y="count", title="Count of diamonds by clarity")
-
-# 2c. Stacked barplot of clarity by cut
-ggplot(diamonds, aes(x=clarity, fill=cut)) +
-  geom_bar() +
-  labs(x="clarity", y="count", title="Stacked barplot of clarity by cut")
-
-# 2d. Proportional stacked barplot of clarity by cut
-ggplot(diamonds, aes(x=clarity, fill=cut)) +
-  geom_bar(position="fill") +
-  labs(x="clarity", y="count", title="Proportional stacked barplot of clarity by cut")
-
-# 2e. Boxplot of price by clarity
-ggplot(diamonds, aes(x=clarity, y=price)) +
-  geom_boxplot() +
-  labs(x="clarity", y="price", title="Boxplot of price by clarity")
-
-
--- Assignment11/Assignment11.R
+++ Assignment11/Assignment11.R
@@ -28,3 +28,34 @@ print(
     geom_smooth(method="lm", se=TRUE, color="blue", inherit.aes=FALSE, aes(x=hwy, y=cty)) +
     labs(title="Scatterplot of hwy vs cty with regression line by manufacturer")
 )
+
+# 2a. Proportion barplot of clarity
+library(ggplot2)
+data(diamonds)
+diamonds_clarity <- as.data.frame(table(diamonds$clarity))
+diamonds_clarity$prop <- diamonds_clarity$Freq / sum(diamonds_clarity$Freq)
+ggplot(diamonds_clarity, aes(x=Var1, y=prop)) +
+  geom_bar(stat="identity", fill="grey40") +
+  labs(x="clarity", y="prop", title="Proportion of diamonds by clarity")
+
+# 2b. Count barplot of clarity colored by clarity
+ggplot(diamonds, aes(x=clarity, fill=clarity)) +
+  geom_bar() +
+  labs(x="clarity", y="count", title="Count of diamonds by clarity")
+
+# 2c. Stacked barplot of clarity by cut
+ggplot(diamonds, aes(x=clarity, fill=cut)) +
+  geom_bar() +
+  labs(x="clarity", y="count", title="Stacked barplot of clarity by cut")
+
+# 2d. Proportional stacked barplot of clarity by cut
+ggplot(diamonds, aes(x=clarity, fill=cut)) +
+  geom_bar(position="fill") +
+  labs(x="clarity", y="count", title="Proportional stacked barplot of clarity by cut")
+
+# 2e. Boxplot of price by clarity
+ggplot(diamonds, aes(x=clarity, y=price)) +
+  geom_boxplot() +
+  labs(x="clarity", y="price", title="Boxplot of price by clarity")
+
+
