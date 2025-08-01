17825ffc773a19d4262e1f2ce1d4e58806f01f5d
JudahA72 <Judahalter@gmail.com>
2025-08-01 15:37:57 -0400

    update for assignment 11

--- Assignment11/Assignment11.R
+++ Assignment11/Assignment11.R
@@ -1,23 +1,22 @@
 # Name: Assignment11.R
-# Description: This script answers basic R syntax and operations from Assignment 10.
+# Description: This script answers basic R syntax and operations from Assignment 11.
 # All work is completed by Judah Alter.
 
-
-# 1a. Scatterplot of hwy vs cty using ggplot2 and mpg dataset
 library(ggplot2)
-data(mpg)
-ggplot(mpg, aes(x=hwy, y=cty)) +
-  geom_point()
+data(mpg) # Load the mpg dataset
 
-# 1b. Scatterplot of hwy vs cty colored by manufacturer
-library(ggplot2)
-data(mpg)
-ggplot(mpg, aes(x=hwy, y=cty, color=manufacturer)) +
-  geom_point()
-
-# 1c. Faceted scatterplot of hwy vs cty by manufacturer
-library(ggplot2)
-data(mpg)
+# 1a. Scatterplot of hwy vs cty
 ggplot(mpg, aes(x=hwy, y=cty)) +
   geom_point() +
-  facet_wrap(~ manufacturer)
+  labs(title="Scatterplot of hwy vs cty")
+
+# 1b. Scatterplot colored by manufacturer
+ggplot(mpg, aes(x=hwy, y=cty, color=manufacturer)) +
+  geom_point() +
+  labs(title="Scatterplot of hwy vs cty colored by manufacturer")
+
+# 1c. Faceted scatterplot by manufacturer
+ggplot(mpg, aes(x=hwy, y=cty)) +
+  geom_point() +
+  facet_wrap(~ manufacturer) +
+  labs(title="Faceted scatterplot of hwy vs cty by manufacturer")
--- Assignment11/Assignment11.py
+++ Assignment11/Assignment11.py
@@ -2,23 +2,28 @@
 # Description: This script answers basic python syntax and operations from Assignment 11.
 # All work is completed by Judah Alter.
 
-# 1a. Scatterplot of hwy vs cty using seaborn and the mpg dataset
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 mpg = sns.load_dataset('mpg')
-sns.scatterplot(x='hwy', y='cty', data=mpg)
-plt.xlabel('hwy')
-plt.ylabel('cty')
+
+# 1a. Scatterplot of horsepower vs mpg
+sns.scatterplot(x='horsepower', y='mpg', data=mpg)
+plt.xlabel('horsepower')
+plt.ylabel('mpg')
+plt.title('Scatterplot of horsepower vs mpg')
 plt.show()
 
-# 1b. Scatterplot of hwy vs cty colored by manufacturer
-sns.scatterplot(x='hwy', y='cty', hue='manufacturer', data=mpg)
-plt.xlabel('hwy')
-plt.ylabel('cty')
+# 1b. Scatterplot colored by origin (similar to manufacturer)
+sns.scatterplot(x='horsepower', y='mpg', hue='origin', data=mpg)
+plt.xlabel('horsepower')
+plt.ylabel('mpg')
+plt.title('Scatterplot of horsepower vs mpg colored by origin')
 plt.show()
 
-# 1c. Faceted scatterplot of hwy vs cty by manufacturer
-g = sns.FacetGrid(mpg, col='manufacturer', col_wrap=4)
-g.map_dataframe(sns.scatterplot, x='hwy', y='cty')
-g.set_axis_labels('hwy', 'cty')
+# 1c. Faceted scatterplot by origin
+g = sns.FacetGrid(mpg, col='origin', col_wrap=3)
+g.map_dataframe(sns.scatterplot, x='horsepower', y='mpg')
+g.set_axis_labels('horsepower', 'mpg')
+g.fig.suptitle('Faceted scatterplot of horsepower vs mpg by origin', y=1.03)
 plt.show()
